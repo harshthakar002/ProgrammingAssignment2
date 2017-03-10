69bae5ca14162274e39acbec3f3c61ae00726386
Harsh Thakar <harshthakar002@gmail.com>
2017-03-10 18:59:51 +0530

    Adding logic

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are useful for creating a matrix where the inverse will be cached
 
-## Write a short comment describing this function
-
+## This functions creates a matrix with a cacheable inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse if it is cached. If it is not cached it
+## will calculate the inverse and then cache it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data) %*% data
+  x$setinverse(i)
+  i
 }
